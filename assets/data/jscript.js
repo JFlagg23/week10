dc8722fad05b5df0290fb455d642ec2ff5b50efb
Justin Flagg <jus2200910@maricopa.edu>
2023-04-04 13:54:52 -0700

    week10End

--- assets/data/jscript.js
+++ assets/data/jscript.js
@@ -31,3 +31,41 @@ function getMenu() {
         nav.appendChild(li);
     }
 }
+
+function getSemester() {
+    // setup variables
+    var semesterId = document.getElementById('semesterList');
+    var option = document.createElement('option');
+
+    // add placeholder option
+    option.value = 0;
+    option.text = 'Select a semester';
+    semesterId.add(option);
+
+    // populate semester list from array
+    for (var x = 0;  x < arySemesters.length; x++) {
+         opt = document.createElement('option');
+         opt.value = arySemesters[x][0];
+         opt.text = arySemesters[x][1];
+         semesterId.add(opt);
+    }
+}
+
+function getClass(objSemester) {
+    // setup variables
+    var semesterId = objSemester.options[objSemester.selectedIndex].value;
+    var aryClasses = eval(`ary${semesterId}`);
+    var classId = document.getElementById('classList');
+    var option = document.createElement('option');
+
+    // populate class list from array
+    for (var x = 0;  x < aryClasses.length; x++) {
+        opt = document.createElement('option');
+        opt.value = aryClasses[x][0];
+        opt.text = aryClasses[x][1];
+        classId.add(opt);
+   }
+
+
+}
+
